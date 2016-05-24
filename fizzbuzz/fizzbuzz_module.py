9c1bafc8b9cc560650fc651c54015243be69a0ac
Leviathan11 <gubik.levente@gmail.com>
2016-05-24 09:43:30 +0200

    Innitial versions from yesterday

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,23 @@
 def fizzbuzz(number):
-    return
+    if (float(number) / 15) == round(number / 15) :
+        sign = "FizzBuzz"
+    elif (float(number) / 3) == round(number / 3) :
+        sign = "Fizz"
+    elif (float(number) / 5) == round(number / 5) :
+        sign = "Buzz"
+    else :
+        sign = number
+    print(sign)
+    return sign
+
 
 
 def main():
+    for x in range(1, 101):
+        fizzbuzz(x)
     return
+
+
 
 if __name__ == '__main__':
     main()
--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,25 @@
-def palindrome(str):
-    return
+def palindrome(str1):
+
+    str_list = list(str1)
+    for char in str_list :
+        if char == " " :
+            str_list.remove(char)
+    str2 = "".join(str_list)
+    print (str2)
+    if str2.lower() == (str(str2[::-1])).lower() :
+        statement = True
+    else :
+        statement = False
+
+    return statement
 
 
 def main():
+    is_palindrome = str(input("Give me a word please: "))
+    if palindrome(is_palindrome) == True :
+        print("No, of course this is not a palindrome, according to the unittest You wrote :)")
+    else :
+        print("Yes, this is a beautiful palindrome according to the unittest You wrote :)")
     return
 
 
