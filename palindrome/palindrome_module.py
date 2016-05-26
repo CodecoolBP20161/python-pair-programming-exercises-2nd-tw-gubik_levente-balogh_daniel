67c232db5e893d3eee0832672710bbda7bfffa7c
Leviathan11 <gubik.levente@gmail.com>
2016-05-26 09:30:52 +0200

    passwordgenerator ready

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,25 +1,14 @@
-def palindrome(str1):
-
-    str_list = list(str1)
-    for char in str_list :
-        if char == " " :
-            str_list.remove(char)
-    str2 = "".join(str_list)
-    print (str2)
-    if str2.lower() == (str(str2[::-1])).lower() :
-        statement = True
-    else :
-        statement = False
-
-    return statement
+def palindrome(strng):
+    strng = "".join(strng.split())
+    return strng.lower() == strng[::-1].lower()
 
 
 def main():
     is_palindrome = str(input("Give me a word please: "))
-    if palindrome(is_palindrome) == True :
-        print("No, of course this is not a palindrome, according to the unittest You wrote :)")
-    else :
+    if palindrome(is_palindrome):
         print("Yes, this is a beautiful palindrome according to the unittest You wrote :)")
+    else:
+        print("No, of course this is not a palindrome, according to the unittest You wrote :)")
     return
 
 
--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,10 +1,43 @@
+import string
+import random
+
+
 def passwordgen():
-    return
+    password = ""
+    LOWER = string.ascii_lowercase
+    UPPER = string.ascii_uppercase
+    DIGITS = string.digits
+    PUNCT = string.punctuation
+    n = 0
+    chars = [1, 1, 1, 1]
+    while n < 8 or (chars[0] or chars[1] or chars[2] or chars[3]):
+        rnd = random.randrange(0, 4)
+        if rnd == 0:
+            indx = random.randrange(0, len(LOWER))
+            password += LOWER[indx]
+            chars[rnd] = 0
+        elif rnd == 1:
+            indx = random.randrange(0, len(UPPER))
+            password += UPPER[indx]
+            chars[rnd] = 0
+        elif rnd == 2:
+            indx = random.randrange(0, len(DIGITS))
+            password += DIGITS[indx]
+            chars[rnd] = 0
+        elif rnd == 3:
+            indx = random.randrange(0, len(PUNCT))
+            password += PUNCT[indx]
+            chars[rnd] = 0
+        n += 1
+    return password
 
 
 def main():
-    return
+    print (passwordgen())
 
+main()
 
+'''
 if __name__ == '__main__':
     main()
+'''
